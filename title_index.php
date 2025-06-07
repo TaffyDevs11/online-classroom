b73585c7aa87e0c2f53f8c8ab7927d7eb7f0a5ea
Daniel Kamanga <155548093+TaffyDevs11@users.noreply.github.com>
2025-06-07 07:31:36 +0200

    Add files via upload

---
+++ title_index.php
@@ -0,0 +1,38 @@
+				
+								<div class="row-fluid">
+
+						<div class="span12">
+						
+						</div>	
+													
+							</div>
+							<div class="row-fluid">
+
+						<div class="span4">
+						<img class="index_logo" src="img/logo.png">
+						</div>	
+						<div class="span8">
+						
+								<div class="title">
+							<p class="chmsc">Excellence in Education</p>
+							<h3>
+
+							<p>AppGrade Academy</p>
+						
+							</h3>		
+						</div>
+			
+						</div>							
+							</div>
+				
+				<div class="row-fluid">
+
+						<div class="span12">
+						<br>
+								<div class="motto">
+												<p>AppGrade:</p>
+												<p>Excellence, Competence and Educational</p>
+												<p>Leadership in IELTS</p>
+								</div>		
+						</div>		
+				</div>
